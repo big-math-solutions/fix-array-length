c8c8b4b909c3505822014aa61e170d5190786638
cereceres <jesus.edelcereceres@gmail.com>
2017-11-29 11:04:09 -0600

    the symbol is added to encapsule the array

--- index.js
+++ index.js
@@ -1,78 +1,74 @@
 const fixList = function(len = 0) {
+    const arraySymbol = Symbol('_array');
     if (!(this instanceof fixList)) return new fixList(len);
+    this.length = len;
     this._len = len;
-    this._array = [];
+    this[arraySymbol] = [];
     this.push = (...items) => {
-        if (items.length === 1) return this._pushOne(items[0])
+        for (let i = 0; i < items.length; i++) this._pushOne(items[i]);
 
-        const length = this._array.length + items.length;
-        if (length <= this._len) return this._array.push(...items);
-
-        this._array.push(...items)
-        this._array = this._array.slice(-this._len)
-
-        return this
+        return this;
     };
 
     this._pushOne = (item) => {
-        this._array.push(item)
+        this[arraySymbol].push(item);
 
-        if (this._array.length > this._len) return this._array.shift()
+        if (this[arraySymbol].length > this._len) return this[arraySymbol].shift();
 
-        return this
+        return this;
     };
 
-    this.pull = this._array.pop;
+    this.pull = this[arraySymbol].pop;
 
     this.concat = (...arg) => {
-        this._array = this._array.concat(...arg).slice(-this._len);
+        this[arraySymbol] = this[arraySymbol].concat(...arg).slice(-this._len);
         return this;
     };
 
     this.leftConcat = (...arg) => {
-        this._array = arg[0]
-            .concat(arg[0], ...arg.slice(1).push(this._array))
+        this[arraySymbol] = arg[0]
+            .concat(arg[0], ...arg.slice(1).push(this[arraySymbol]))
             .slice(-this._len);
         return this;
     };
 
-    this.from = (_array) => {
-        const array = Array.isArray(_array) ? _array : [ _array ];
-        this._array = array.slice(0, len);
+    this.from = (arrayArg) => {
+        const array = Array.isArray(arrayArg) ? arrayArg : [ arrayArg ];
+        this[arraySymbol] = array.slice(0, len);
         return this;
     };
-    this.map = this._array.map;
-    this.filter = this._array.filter;
-    this.forEach = this._array.forEach;
-    this.pop = this._array.pop;
-    this.shift = this._array.shift;
+    this.map = this[arraySymbol].map;
+    this.filter = this[arraySymbol].filter;
+    this.forEach = this[arraySymbol].forEach;
+    this.pop = this[arraySymbol].pop;
+    this.shift = this[arraySymbol].shift;
     this.unshift = (...items) => {
-        const length = this._array.length + items.length;
+        const length = this[arraySymbol].length + items.length;
 
-        if (length <= this._len) return this._array.unshift(...items);
+        if (length <= this._len) return this[arraySymbol].unshift(...items);
 
-        for(let i = 0; i < length - this._len; i++) this._array.pop();
+        for(let i = 0; i < length - this._len; i++) this[arraySymbol].pop();
 
-        return this._array.unshift(...items);
+        return this[arraySymbol].unshift(...items);
     };
-    this.get = (i) => this._array[i];
+    this.get = (i) => this[arraySymbol][i];
     this.set = (i, item) => {
-        if (i < this._len) this._array[i] = item;
+        if (i < this._len) this[arraySymbol][i] = item;
         return this;
     };
-    this.getList = this._array.valueOf;
-    this.slice = this._array.slice;
-    this.toString = this._array.toString;
-    this.sort = this._array.sort;
-    this.join = this._array.join;
-    this.find = this._array.find;
-    this.findIndex = this._array.findIndex;
-    this.indexOf = this._array.indexOf;
-    this.lastIndexOf = this._array.lastIndexOf;
-    this.reduce = this._array.reduce;
-    this.reduceRight = this._array.reduceRight;
-    this.reverse = this._array.reverse;
-    this.valueOf = this._array.valueOf;
+    this.getList = () => this[arraySymbol].valueOf();
+    this.slice = this[arraySymbol].slice;
+    this.toString = this[arraySymbol].toString;
+    this.sort = this[arraySymbol].sort;
+    this.join = this[arraySymbol].join;
+    this.find = this[arraySymbol].find;
+    this.findIndex = this[arraySymbol].findIndex;
+    this.indexOf = this[arraySymbol].indexOf;
+    this.lastIndexOf = this[arraySymbol].lastIndexOf;
+    this.reduce = this[arraySymbol].reduce;
+    this.reduceRight = this[arraySymbol].reduceRight;
+    this.reverse = this[arraySymbol].reverse;
+    this.valueOf = () => this[arraySymbol].valueOf();
 };
 
 module.exports = fixList;
--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ const list = new List(2);
 describe('test to list with fix length', () => {
     it('should return every function', () => {
         for (const method in list) {
-            if (method === '_len' || method === '_array') continue;
+            if (method === '_len' || method === 'length') continue;
             assert(typeof list[method] === 'function');
         }
     });
@@ -15,36 +15,36 @@ describe('test to list with fix length', () => {
         list.push(0);
         list.push(1);
         list.push(2);
-        assert(list._array.length === 2);
-        assert.deepStrictEqual(list._array, [ 1, 2 ]);
+        assert(list.valueOf().length === 2);
+        assert.deepStrictEqual(list.valueOf(), [ 1, 2 ]);
     });
 
     it('should fix the length in push recursive', () => {
         list.push(0, 1, 2, 3, 4);
-        assert(list._array.length === 2);
-        assert.deepStrictEqual(list._array, [ 3, 4 ]);
+        assert(list.valueOf().length === 2);
+        assert.deepStrictEqual(list.valueOf(), [ 3, 4 ]);
     });
 
     it('should fix the length in unshift', () => {
         list.unshift(2);
         list.unshift(4);
         list.unshift(5);
-        assert(list._array.length === 2);
-        assert.deepStrictEqual(list._array, [ 5, 4 ]);
+        assert(list.valueOf().length === 2);
+        assert.deepStrictEqual(list.valueOf(), [ 5, 4 ]);
     });
 
     it('should fix the length in unshift recursive', () => {
         list.unshift(2);
         list.unshift(4);
         list.unshift(5);
-        assert(list._array.length === 2);
-        assert.deepStrictEqual(list._array, [ 5, 4 ]);
+        assert(list.valueOf().length === 2);
+        assert.deepStrictEqual(list.valueOf(), [ 5, 4 ]);
     });
 
     it('should fix the length in concat', () => {
         list.concat([ 1, 2, 3, 4, 4, 5, 6, 7, 8 ]);
-        assert(list._array.length === 2);
-        assert.deepStrictEqual(list._array, [ 7, 8 ]);
+        assert(list.valueOf().length === 2);
+        assert.deepStrictEqual(list.valueOf(), [ 7, 8 ]);
     });
 
     it('should return the element with get', () => assert(list.get(1) === 8));
